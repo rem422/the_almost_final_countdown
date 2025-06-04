390a6dcff7cf48d41954e982d887659f26a13f75
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2025-06-04 23:59:12 +0300

    Almost Final Countdown

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import {forwardRef, useImperativeHandle, useRef} from 'react'
 
-const ResultModal = ({ref, result, targetTime}) => {
-  return <dialog ref={ref} className='result-modal'>
+const ResultModal = forwardRef( function ResultModal({result, targetTime}, ref) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+        open() {
+            dialog.current.showModal();
+        }
+    }
+  });
+
+  return(
+    <dialog ref={dialog} className='result-modal'>
     <h2>You {result}</h2>
     <p>The target time was <strong>{targetTime} seconds</strong></p>
     <p>You stopped the timer with <strong>X seconds left.</strong></p>
@@ -9,6 +20,7 @@ const ResultModal = ({ref, result, targetTime}) => {
         <button>Close</button>
     </form>
    </dialog>
-}
+  ) 
+})
 
 export default ResultModal
