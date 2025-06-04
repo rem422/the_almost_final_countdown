89e25a314786e05842aa6497cfffe61edf003c60
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2025-06-04 22:07:09 +0300

    The Almost Final CountDown

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useRef } from 'react'
+// let timer;
+
+const TimerChallenge = ({title, targetTime}) => {
+    const timer = useRef();
+    const [timerExpired, setTimerExpited] = useState(false);
+    const [timerStarted, setTimerStarted] = useState(false);
+
+
+const handleStart = () => {
+    timer.current = setTimeout(() => {
+        setTimerExpited(true)
+    }, targetTime * 1000);
+
+    setTimerStarted(true);
+}
+
+const handleStop = () => {
+    clearTimeout(timer.current);
+}
+
+  return (
+    <section className='challenge'>
+        <h2>{title}</h2>
+        {timerExpired && <p>You lost!</p>}
+        <p className='challenge-time'>
+            {targetTime} second{targetTime > 1 ? 's' : ''}
+        </p>
+        <p>
+            <button onClick={timerStarted ? handleStop : handleStart}>
+                {timerStarted ? "Stop" : 'Start'} Challenge
+            </button>
+        </p>
+        <p className= {timerStarted ? "active" : undefined}>
+            {timerStarted ? "Time is running... " :  "Timer inactive"}
+        </p>
+    </section>
+  )
+}
+
+export default TimerChallenge
